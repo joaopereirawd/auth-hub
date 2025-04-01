99b31c9e0a590f181b69be1eddf0ac9f8e49b844
joaopereira <joao.pedro.pereira@inetum.com>
2025-04-01 17:17:15 +0100

    initialize React + TypeScript + Vite project with basic structure and configurations

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const LoginForm = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="pw" />
+        </form>
+    )
+}
